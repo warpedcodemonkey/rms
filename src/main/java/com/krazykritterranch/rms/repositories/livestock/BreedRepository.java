ca20eb1bbb65286e83d3b4e094c638de80aae1fb
John B <warpedcodemonkey@yahoo.com>
2025-06-14 18:04:34 -0400

    Restarting the design

--- src/main/java/com/krazykritterranch/rms/repositories/livestock/BreedRepository.java
+++
@@ -1,9 +0,0 @@
-package com.krazykritterranch.rms.repositories.livestock;
-
-import com.krazykritterranch.rms.model.livestock.Breed;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BreedRepository extends JpaRepository<Breed, Long> {
-}
